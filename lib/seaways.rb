7baa47e3ba3240885ce368f9484c35e9c1b084ca
Nick Platt <platt.nicholas@gmail.com>
2014-05-17 19:34:15 -0400

    Specify #make_uri. Fix indentation.

--- lib/seaways.rb
+++ lib/seaways.rb
@@ -12,130 +12,130 @@ $stdout.sync
 
 module Seaways
   CONFIG = {
-    blacklist: %w(.zip .jpg .jpeg .gif .png .eps),
-    debug: false,
+   blacklist: %w(.zip .jpg .jpeg .gif .png .eps),
+   debug: false,
   }
 
   def self.navigate(href)
-    Core.new(href).run
+   Core.new(href).run
   end
 
   class Core
-    attr_accessor :pages, :errors
-    def initialize(host)
-      @pages = {}
-      @errors = []
+   attr_accessor :pages, :errors
+   def initialize(host)
+    @pages = {}
+    @errors = []
 
-      host = 'http://' << host unless host.start_with?('http://', 'https://')
-      @target = make_uri(host)
-      @queue = [@target.to_s]
-    end
+    host = 'http://' << host unless host.start_with?('http://', 'https://')
+    @target = make_uri(host)
+    @queue = [@target.to_s]
+   end
 
-    def run
-      while !@queue.empty?
-        status
-        @href = @queue.shift
-        visit(@href) unless visited(@href)
-      end
+   def run
+    while !@queue.empty?
       status
-      puts nil, @pages.to_yaml, @errors.sort.to_yaml
+      @href = @queue.shift
+      visit(@href) unless visited(@href)
     end
+    status
+    puts nil, @pages.to_yaml, @errors.sort.to_yaml
+   end
 
-    def status
-      $stderr.printf("\rPages: %3d   Queue: %3d   Errors: %3d",
-              @pages.size, @queue.size, @errors.size)
+   def status
+    $stderr.printf("\rPages: %3d  Queue: %3d  Errors: %3d",
+          @pages.size, @queue.size, @errors.size)
+   end
+
+   def visit(href)
+    if doc = get(href)
+      links = links(doc)
+      assets = assets(doc)
+      @pages[href.to_sym] = {
+       links: links,
+       assets: assets,
+      }
+      @queue += links[:local].select { |h| !visited(h) }
+    else
+      @pages[href.to_sym] = nil
     end
+   end
 
-    def visit(href)
-      if doc = get(href)
-        links = links(doc)
-        assets = assets(doc)
-        @pages[href.to_sym] = {
-          links: links,
-          assets: assets,
-        }
-        @queue += links[:local].select { |h| !visited(h) }
-      else
-        @pages[href.to_sym] = nil
+   def visited(href)
+    @pages.key?(href.to_sym)
+   end
+
+   def get(href, tries=0)
+    puts "get #{ href }" if CONFIG[:debug]
+    Nokogiri::HTML(open(href))
+   rescue RuntimeError, OpenURI::HTTPError => error
+    if tries >= 5
+      @errors << "Possible infinite loop: skipping #{ href }"
+    elsif error.message =~ /^redirection forbidden: (http.*) -> (http.*)$/
+      # This is ugly, but open-uri doesn't natively handle redirects.
+      puts "  `- #{$1} -> #{$2}" if CONFIG[:debug]
+      uri = make_uri($2)
+      if uri
+       @href = uri.to_s
+       return get(@href, (tries + 1))
       end
+    else
+      @errors << "Error: #{ error } -- #{ @href }"
     end
+    nil
+   rescue URI::InvalidURIError, Errno::ENOENT => error
+     @errors << "Error: #{ error } -- #{ @href }"
+    nil
+   end
 
-    def visited(href)
-      @pages.key?(href.to_sym)
-    end
+   def links(doc)
+    [:local, :remote].zip(
+      doc.css('a[href]').to_a
+       .map { |a| make_uri(a[:href]) }
+       .compact
+       .partition { |uri| follow_link?(uri) }
+       .map do |list|
+        list
+          .uniq { |uri| uri.to_s }
+          .collect { |uri| uri.to_s }
+          .sort
+       end
+    ).to_h
+   end
 
-    def get(href, tries=0)
-      puts "get #{ href }" if CONFIG[:debug]
-      Nokogiri::HTML(open(href))
-    rescue RuntimeError, OpenURI::HTTPError => error
-      if tries >= 5
-        @errors << "Possible infinite loop: skipping #{ href }"
-      elsif error.message =~ /^redirection forbidden: (http.*) -> (http.*)$/
-        # This is ugly, but open-uri doesn't natively handle redirects.
-        puts "   `- #{$1} -> #{$2}" if CONFIG[:debug]
-        uri = make_uri($2)
-        if uri
-          @href = uri.to_s
-          return get(@href, (tries + 1))
-        end
-      else
-        @errors << "Error: #{ error } -- #{ @href }"
-      end
-      nil
-    rescue URI::InvalidURIError, Errno::ENOENT => error
-       @errors << "Error: #{ error } -- #{ @href }"
-      nil
-    end
+   def follow_link?(uri)
+    uri.scheme.start_with?('http') &&
+    uri.host.start_with?(@target.host, 'www.' << @target.host) &&
+    !uri.path.end_with?(*CONFIG[:blacklist])
+   end
 
-    def links(doc)
-      [:local, :remote].zip(
-        doc.css('a[href]').to_a
-          .map { |a| make_uri(a[:href]) }
-          .compact
-          .partition { |uri| follow_link?(uri) }
-          .map do |list|
-            list
-              .uniq { |uri| uri.to_s }
-              .collect { |uri| uri.to_s }
-              .sort
-          end
-      ).to_h
-    end
+   def assets(doc)
+    ref = lambda { |node| (node[:src] || node[:href]).to_s }
 
-    def follow_link?(uri)
-      uri.scheme.start_with?('http') &&
-      uri.host.start_with?(@target.host, 'www.' << @target.host) &&
-      !uri.path.end_with?(*CONFIG[:blacklist])
-    end
+    [:js, :css].zip(
+      doc.css('script[src], link[href]').to_a
+       .partition { |node| node[:src] }
+       .map do |list|
+        list
+          .uniq { |node| ref.call(node) }
+          .collect { |node| ref.call(node) }
+          .sort
+       end
+    ).to_h
+   end
 
-    def assets(doc)
-      ref = lambda { |node| (node[:src] || node[:href]).to_s }
-
-      [:js, :css].zip(
-        doc.css('script[src], link[href]').to_a
-          .partition { |node| node[:src] }
-          .map do |list|
-            list
-              .uniq { |node| ref.call(node) }
-              .collect { |node| ref.call(node) }
-              .sort
-          end
-      ).to_h
-    end
-
-    def make_uri(href)
-      uri = URI::parse(href.to_s)
-      uri.normalize!
-      return uri if uri.scheme && !uri.scheme.start_with?('http')
-      uri.scheme ||= @target.scheme
-      uri.host ||= @target.host
-      uri.path = '/' << uri.path unless uri.path.start_with?('/')
-      uri.query = nil
-      uri.fragment = nil
-      uri.freeze
-    rescue URI::InvalidURIError
-      @errors << "Bad URI: #{ str } on page #{ @href }"
-      nil
-    end
+   def make_uri(href)
+    uri = URI::parse(href.to_s)
+    uri.normalize!
+    return uri if uri.scheme && !uri.scheme.start_with?('http')
+    uri.scheme ||= @target.scheme
+    uri.host ||= @target.host
+    uri.path = '/' << uri.path unless uri.path.start_with?('/')
+    uri.query = nil
+    uri.fragment = nil
+    uri
+   rescue URI::InvalidURIError
+    @errors << "Bad URI: #{ str } on page #{ @href }"
+    nil
+   end
   end
 end
--- test/core.rb
+++ test/core.rb
@@ -3,114 +3,144 @@ require_relative '../lib/seaways'
 include Seaways
 
 describe Seaways do
-   before do
-      @seaways = Seaways::Core.new('http://example.org')
-   end
+  before do
+    @seaways = Seaways::Core.new('http://example.org')
+  end
 
-   describe '#visit' do
-      context 'with a valid href' do
-         it 'inserts a page' do
-            @seaways.visit('http://example.org')
-            expect(@seaways.pages).to have_key(:'http://example.org')
-         end
-
-         # Needs document mock; example.org has no local links
-         it 'adds local links to the queue'
+  describe '#visit' do
+    context 'with a valid href' do
+      it 'inserts a page' do
+        @seaways.visit('http://example.org')
+        expect(@seaways.pages).to have_key(:'http://example.org')
       end
 
-      context 'with an invalid href' do
-         it 'inserts a nil page' do
-            @seaways.visit('http://example!org')
-            expect(@seaways.pages).to eql({:'http://example!org' => nil})
-         end
+      # Needs document mock; example.org has no local links
+      it 'adds local links to the queue'
+    end
+
+    context 'with an invalid href' do
+      it 'inserts a nil page' do
+        @seaways.visit('http://example!org')
+        expect(@seaways.pages).to eql({:'http://example!org' => nil})
       end
-   end
+    end
+  end
 
-   describe '#get' do
-      context 'with a valid href' do
-         it 'returns a document'
-         it 'follows redirects'
-         it "doesn't infinitely redirect"
-      end
+  describe '#get' do
+    context 'with a valid href' do
+      it 'returns a document'
+      it 'follows redirects'
+      it "doesn't infinitely redirect"
+    end
 
-      context 'with an invalid href' do
-         it 'records the error' do
-            @seaways.get('http://example!org')
-            expect(@seaways.errors).to have(1).item
-         end
-
-         it 'returns nil' do
-            expect(@seaways.get('http://example!org')).to be_nil
-         end
-      end
-   end
-
-   describe '#links' do
-      it 'separates local and remote URIs'
-   end
-
-   describe '#assets' do
-      it 'separates local and remote assets'
-   end
-
-   describe '#follow_link?' do
-      context 'with a blacklisted href' do
-         it 'returns false' do
-            uri = @seaways.make_uri('http://example.org/image.jpg')
-            expect(@seaways.follow_link?(uri)).to be_false
-         end
+    context 'with an invalid href' do
+      it 'records the error' do
+        @seaways.get('http://example!org')
+        expect(@seaways.errors).to have(1).item
       end
 
-      context 'with an href in the local domain' do
-         it 'returns true' do
-            uri = @seaways.make_uri('http://example.org/foo/bar')
-            expect(@seaways.follow_link?(uri)).to be_true
-         end
+      it 'returns nil' do
+        expect(@seaways.get('http://example!org')).to be_nil
+      end
+    end
+  end
 
-         it 'returns true (www subdomain)' do
-            uri = @seaways.make_uri('http://www.example.org/foo/bar')
-            expect(@seaways.follow_link?(uri)).to be_true
-         end
+  describe '#links' do
+    it 'separates local and remote URIs'
+  end
 
-         it 'returns true (different scheme)' do
-            uri = @seaways.make_uri('https://example.org/foo/bar')
-            expect(@seaways.follow_link?(uri)).to be_true
-         end
+  describe '#assets' do
+    it 'separates local and remote assets'
+  end
+
+  describe '#follow_link?' do
+    context 'with a blacklisted href' do
+      it 'returns false' do
+        uri = @seaways.make_uri('http://example.org/image.jpg')
+        expect(@seaways.follow_link?(uri)).to be_false
+      end
+    end
+
+    context 'with an href in the local domain' do
+      it 'returns true' do
+        uri = @seaways.make_uri('http://example.org/foo/bar')
+        expect(@seaways.follow_link?(uri)).to be_true
       end
 
-      context 'with an href to a remote domain' do
-         it 'returns false' do
-            uri = @seaways.make_uri('http://google.com')
-            expect(@seaways.follow_link?(uri)).to be_false
-         end
-
-         it 'returns false (non-www subdomain)' do
-            uri = @seaways.make_uri('http://static.example.org')
-            expect(@seaways.follow_link?(uri)).to be_false
-         end
-      end
-   end
-
-   describe '#make_uri' do
-      context 'with a valid href' do
-         it 'parses the scheme, host, and path'
-         it 'drops the querystring'
-         it 'drops the fragment'
+      it 'returns true (www subdomain)' do
+        uri = @seaways.make_uri('http://www.example.org/foo/bar')
+        expect(@seaways.follow_link?(uri)).to be_true
       end
 
-      context 'with a partial href' do
-         it 'uses a default scheme'
-         it 'uses a default host'
-         it 'adds a path separator'
+      it 'returns true (different scheme)' do
+        uri = @seaways.make_uri('https://example.org/foo/bar')
+        expect(@seaways.follow_link?(uri)).to be_true
+      end
+    end
+
+    context 'with an href to a remote domain' do
+      it 'returns false' do
+        uri = @seaways.make_uri('http://google.com')
+        expect(@seaways.follow_link?(uri)).to be_false
       end
 
-      context 'with a valid, non-http href' do
-         it 'retruns the uri'
+      it 'returns false (non-www subdomain)' do
+        uri = @seaways.make_uri('http://static.example.org')
+        expect(@seaways.follow_link?(uri)).to be_false
+      end
+    end
+  end
+
+  describe '#make_uri' do
+    context 'with a valid href' do
+      it 'parses the scheme, host, and path' do
+        uri = @seaways.make_uri('http://example.org')
+        expect(uri).to respond_to(:scheme, :host, :path)
+        expect(uri.scheme).to eql('http')
+        expect(uri.host).to eql('example.org')
+        expect(uri.path).to eql('/')
       end
 
-      context 'with an invalid href' do
-         it 'records an error'
-         it 'returns nil'
+      it 'drops the querystring' do
+        uri = @seaways.make_uri('http://example.org/foo?bar=baz')
+        expect(uri).to respond_to(:query)
+        expect(uri.query).to be_nil
       end
-   end
+
+      it 'drops the fragment' do
+        uri = @seaways.make_uri('http://example.org/foo?bar=baz')
+        expect(uri).to respond_to(:fragment)
+        expect(uri.fragment).to be_nil
+      end
+    end
+
+    context 'with a partial href' do
+      it 'uses a default scheme' do
+        uri = @seaways.make_uri('/foo/bar')
+        expect(uri.scheme).to eql('http')
+      end
+
+      it 'uses a default host' do
+        uri = @seaways.make_uri('/foo/bar')
+        expect(uri.host).to eql('example.org')
+      end
+
+      it 'adds a path separator' do
+        uri = @seaways.make_uri('http://example.org')
+        expect(uri.path).to eql('/')
+      end
+    end
+
+    context 'with a valid, non-http href' do
+      it 'returns the uri' do
+        uri = @seaways.make_uri('mailto:admin@example.org')
+        expect(uri.scheme).to eql('mailto')
+      end
+    end
+
+    context 'with an invalid href' do
+      it 'records an error'
+      it 'returns nil'
+    end
+  end
 end
