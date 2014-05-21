45680e99250ce54e4b86f374cba4a33c759fe17b
Nick Platt <platt.nicholas@gmail.com>
2014-05-20 22:09:43 -0400

    s/example/localhost/g

--- spec/core.rb
+++ spec/core.rb
@@ -26,28 +26,28 @@ describe Seaways do
           </body>
         </html>
       html
+    end
   end
 
   describe '#visit' do
     context 'with a valid href' do
       it 'inserts a page' do
-        @seaways.visit('http://example.org')
-        expect(@seaways.pages).to have_key(:'http://example.org')
+        @seaways.visit('http://localhost.org')
+        expect(@seaways.pages).to have_key(:'http://localhost.org')
       end
 
-      # Needs document mock; example.org has no local links
+      # Needs document mock; localhost.org has no local links
       it 'adds local links to the queue' do
-          @seaways.visit('http://example.org')
-          expect(@seaways.queue).to include('http://example.org/foo/bar')
+          @seaways.visit('http://localhost.org')
+          expect(@seaways.queue).to include('http://localhost.org/foo/bar/')
           expect(@seaways.queue).to have(1).item
-        end
       end
     end
 
     context 'with an invalid href' do
       it 'inserts a nil page' do
-        @seaways.visit('http://example!org')
-        expect(@seaways.pages).to eql({:'http://example!org' => nil})
+        @seaways.visit('http://localhost!org')
+        expect(@seaways.pages).to eql({:'http://localhost!org' => nil})
       end
     end
   end
@@ -61,12 +61,12 @@ describe Seaways do
 
     context 'with an invalid href' do
       it 'records the error' do
-        @seaways.get('http://example!org')
+        @seaways.get('http://localhost!org')
         expect(@seaways.errors).to have(1).item
       end
 
       it 'returns nil' do
-        expect(@seaways.get('http://example!org')).to be_nil
+        expect(@seaways.get('http://localhost!org')).to be_nil
       end
     end
   end
@@ -74,15 +74,15 @@ describe Seaways do
   describe '#links' do
     it 'separates local and remote URIs' do
       links = [
-        { href: 'http://example.org' },
-        { href: 'http://google.com' },
+        { href: 'http://localhost.org' },
+        { href: 'http://remotehost.com' },
       ]
       doc = double
       doc.stub(css: links)
 
       expect(@seaways.links(doc)).to eq(
-        local: ['http://example.org/'],
-        remote: ['http://google.com/']
+        local: ['http://localhost.org/'],
+        remote: ['http://remotehost.com/']
       )
     end
   end
@@ -106,36 +106,36 @@ describe Seaways do
   describe '#follow_link?' do
     context 'with a blacklisted href' do
       it 'returns false' do
-        uri = @seaways.make_uri('http://example.org/image.jpg')
+        uri = @seaways.make_uri('http://localhost.org/image.jpg')
         expect(@seaways.follow_link?(uri)).to be_false
       end
     end
 
     context 'with an href in the local domain' do
       it 'returns true' do
-        uri = @seaways.make_uri('http://example.org/foo/bar')
+        uri = @seaways.make_uri('http://localhost.org/foo/bar')
         expect(@seaways.follow_link?(uri)).to be_true
       end
 
       it 'returns true (www subdomain)' do
-        uri = @seaways.make_uri('http://www.example.org/foo/bar')
+        uri = @seaways.make_uri('http://www.localhost.org/foo/bar')
         expect(@seaways.follow_link?(uri)).to be_true
       end
 
       it 'returns true (different scheme)' do
-        uri = @seaways.make_uri('https://example.org/foo/bar')
+        uri = @seaways.make_uri('https://localhost.org/foo/bar')
         expect(@seaways.follow_link?(uri)).to be_true
       end
     end
 
     context 'with an href to a remote domain' do
       it 'returns false' do
-        uri = @seaways.make_uri('http://google.com')
+        uri = @seaways.make_uri('http://remotehost.com')
         expect(@seaways.follow_link?(uri)).to be_false
       end
 
       it 'returns false (non-www subdomain)' do
-        uri = @seaways.make_uri('http://static.example.org')
+        uri = @seaways.make_uri('http://static.localhost.org')
         expect(@seaways.follow_link?(uri)).to be_false
       end
     end
@@ -144,21 +144,21 @@ describe Seaways do
   describe '#make_uri' do
     context 'with a valid href' do
       it 'parses the scheme, host, and path' do
-        uri = @seaways.make_uri('http://example.org')
+        uri = @seaways.make_uri('http://localhost.org')
         expect(uri).to respond_to(:scheme, :host, :path)
         expect(uri.scheme).to eql('http')
-        expect(uri.host).to eql('example.org')
+        expect(uri.host).to eql('localhost.org')
         expect(uri.path).to eql('/')
       end
 
       it 'drops the querystring' do
-        uri = @seaways.make_uri('http://example.org/foo?bar=baz')
+        uri = @seaways.make_uri('http://localhost.org/foo?bar=baz')
         expect(uri).to respond_to(:query)
         expect(uri.query).to be_nil
       end
 
       it 'drops the fragment' do
-        uri = @seaways.make_uri('http://example.org/foo?bar=baz')
+        uri = @seaways.make_uri('http://localhost.org/foo?bar=baz')
         expect(uri).to respond_to(:fragment)
         expect(uri.fragment).to be_nil
       end
@@ -172,30 +172,30 @@ describe Seaways do
 
       it 'uses a default host' do
         uri = @seaways.make_uri('/foo/bar')
-        expect(uri.host).to eql('example.org')
+        expect(uri.host).to eql('localhost.org')
       end
 
       it 'adds a path separator' do
-        uri = @seaways.make_uri('http://example.org')
+        uri = @seaways.make_uri('http://localhost.org')
         expect(uri.path).to eql('/')
       end
     end
 
     context 'with a valid, non-http href' do
       it 'returns the uri' do
-        uri = @seaways.make_uri('mailto:admin@example.org')
+        uri = @seaways.make_uri('mailto:admin@localhost.org')
         expect(uri.scheme).to eql('mailto')
       end
     end
 
     context 'with an invalid href' do
       it 'records an error' do
-        uri = @seaways.make_uri('http://example!org')
+        uri = @seaways.make_uri('http://localhost!org')
         expect(@seaways.errors).to have(1).item
       end
 
       it 'returns nil' do
-        uri = @seaways.make_uri('http://example!org')
+        uri = @seaways.make_uri('http://localhost!org')
         expect(uri).to be_nil
       end
     end
